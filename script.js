22886509e9b3a19d2698d498553184922a4d60ea
iolakk <170725865+iolakk@users.noreply.github.com>
2024-06-21 23:11:48 +0200

    Add changeable grid size that matches to draw-screen

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++){
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let x = 0; x < 16; x++){
-        let square = document.createElement("div");
-        square.classList.add("square");
 
-        row.appendChild(square);
+const drawScreen = document.querySelector(".draw-screen")
+
+const gridSizeButton = document.querySelector("#grid-size-button");
+
+let gridSize = 0;
+
+function generateSquares(size){
+    container.textContent = "";
+    for (let i = 0; i < size; i++){
+        //let row = document.createElement("div");
+        //row.classList.add("row");
+        for (let x = 0; x < size; x++){
+            let square = document.createElement("div");
+            square.style.width = String(parseInt(window.getComputedStyle(container).width) / size) + "px";
+            square.style.height = String(parseInt(window.getComputedStyle(container).width) / size) + "px";
+            square.classList.add("square");
+            
+            container.appendChild(square);
+        }
+        //container.appendChild(row);
     }
-    container.appendChild(row);
+ 
 }
+
+gridSizeButton.addEventListener("click", () => {
+    gridSize = +prompt("Number of squares per side: ");
+    generateSquares(gridSize);
+});
+
+generateSquares(16);
 
 container.addEventListener("mouseover", (elem) => {
     let hoveredSquare = elem.target
@@ -18,5 +38,4 @@ container.addEventListener("mouseover", (elem) => {
     if (hoveredSquare.getAttribute("class") == "square"){
         hoveredSquare.style["background-color"] = "red";
     }
-    
-})
+});

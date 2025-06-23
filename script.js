8ae5024272ac2e73736f5d2b4ca2d8502c0680be
iolakk <170725865+iolakk@users.noreply.github.com>
2025-06-23 18:12:35 +0200

    Make it so squares are in a grid

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const squareContainer = document.querySelector('#square-container');
 function createSquares() {
-    for (let i = 1; i <= 32; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        squareContainer.appendChild(square);
+    for (let i = 1; i <= 16; i++) {
+        // create a column
+        const column = document.createElement('div');
+        column.classList.add('column');
+        squareContainer.appendChild(column);
+
+        // create a row
+        for (let i = 1; i <= 16; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            column.appendChild(square);
+        }
     }
 }
 

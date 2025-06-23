cd0587999804b6df3800a9baf2f3740e157553f7
iolakk <170725865+iolakk@users.noreply.github.com>
2025-06-23 18:23:18 +0200

    Position Etch-a-Sketch in the center

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function colorSquare(target) {
     target.style['background-color'] = 'red';
 }
 
-function createSquares() {
-    for (let i = 1; i <= 16; i++) {
+function createSquares(x = 16, y = 16) {
+    for (let i = 1; i <= x; i++) {
         // create a column
         const column = document.createElement('div');
         column.classList.add('column');
         squareContainer.appendChild(column);
 
         // create a row
-        for (let i = 1; i <= 16; i++) {
+        for (let i = 1; i <= y; i++) {
             const square = document.createElement('div');
             square.classList.add('square');
             column.appendChild(square);

8624b606cbef898e5560d8d2b727de9af830f3d7
iolakk <170725865+iolakk@users.noreply.github.com>
2025-06-23 18:19:31 +0200

    Add coloring squares on hover

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const squareContainer = document.querySelector('#square-container');
+
+function colorSquare(target) {
+    target.style['background-color'] = 'red';
+}
+
 function createSquares() {
     for (let i = 1; i <= 16; i++) {
         // create a column
@@ -11,6 +16,14 @@ function createSquares() {
             const square = document.createElement('div');
             square.classList.add('square');
             column.appendChild(square);
+
+            square.addEventListener('mouseover', (e) => {
+                /* 
+                send over event's target (that will be the square we are currently hovering over)
+                to the colorSquare() function
+                */
+                colorSquare(e.target);
+            })
         }
     }
 }
